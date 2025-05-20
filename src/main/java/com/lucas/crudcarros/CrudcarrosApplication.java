4227263b38684944822ce50a31442d1bb9fbf512
Lucas Oliveira Silvério <oliveiratricolorfc@hotmail.com>
2025-05-20 20:56:01 -0300

    Criando Projeto

---
+++ src/main/java/com/lucas/crudcarros/CrudcarrosApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.crudcarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudcarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudcarrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/crudcarros/CrudcarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.crudcarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudcarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
